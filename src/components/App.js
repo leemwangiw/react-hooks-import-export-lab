7340144d4b6ebecdb0099783c81f07916f1c25dc
leemwangiw <lee.mwangi1@student.moringaschool.com>
2024-08-03 10:34:47 +0300

    hehe

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import NavBar from "./NavBar"; // Use relative path if in the same directory
+import Home from "./Home";
+import About from "./About";
 
 function App() {
   return (
